997f6fd53ce40d10bbb8fa340ef6907d6c5c025b
Abhay'sRepo <abhayrajchauhan.976@gmail.com>
2025-08-24 23:21:22 +0530

    initial commut

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <strong>this is a urlshortner App</strong>
+    </div>
+  );
+}
+
+export default App;
