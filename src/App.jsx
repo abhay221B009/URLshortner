965035b991baa40a6ad0573bcd77782f4010d0fd
Abhay'sRepo <abhayrajchauhan.976@gmail.com>
2025-08-26 22:43:16 +0530

    infused shadcn ui

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import React from "react";
-
+import Button from "./components/ui/Button";
 import "./App.css";
 
 function App() {
   return (
     <div>
       <strong>this is a urlshortner App</strong>
+      <Button>Button</Button>
     </div>
   );
 }
---
+++ src/components/ui/Button.jsx
@@ -0,0 +1,8 @@
+// src/components/Button.jsx
+export default function Button({ children }) {
+  return (
+    <button className="px-4 py-2 bg-blue-500 text-white rounded-lg hover:bg-blue-600">
+      {children}
+    </button>
+  );
+}
